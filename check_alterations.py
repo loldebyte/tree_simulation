01add4ea2ac0c8e7e5f860e74224ab00511d5232
loldebyte <qdimarellis@laposte.net>
2022-07-03 17:44:43 +0200

    refacto: split df into 3 separate dfs, one by tier concerned

--- check_alterations.py
+++ check_alterations.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
+"""Asserts that all combinations of alterations are represented and coherent.
+
 Created on Wed Jun 29 16:48:35 2022
 
 @author: loldebyte
 """
 import pandas as pd
+import itertools
 
 
 f = "/media/hdd/Downloads/Modifications d'État - Arbre des Altérations.csv"
@@ -22,9 +22,14 @@ cols_to_ffill = ([f"Tier {x} Altération d'État" for x in range(1, 4)]
                  + [f"Tier {x} Réactif 1" for x in range(1, 4)])
 df[cols_to_ffill] = df[cols_to_ffill].fillna(method="ffill")
 
+t1 = df.iloc[:, :4].dropna(subset="Tier 1 Produit")
+t2 = df.iloc[:, 4:8].dropna(subset="Tier 2 Produit")
+t3 = df.iloc[:, 8:12].dropna(subset="Tier 3 Produit")
+
 
 def check_all_combinations() -> bool:
     """Return True if all combinations of reagents exist in the dataframe."""
+    # TODO: itertools.combinations()
     return False
 
 
