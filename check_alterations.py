66a0fdc5007a2c82f56b82e654b645f1a29e0a41
loldebyte <qdimarellis@laposte.net>
2023-01-16 16:19:34 +0100

    feat: search for corresponding series in all tiers
    fix: use locale to accurately compare accented strings

--- check_alterations.py
+++ check_alterations.py
@@ -6,8 +6,12 @@ Created on Wed Jun 29 16:48:35 2022
 """
 import pandas as pd
 import itertools
+import locale
+import functools
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
+locale.setlocale(locale.LC_ALL, "fr_FR.UTF-8")
 
 
 f = "./Modifications d'État - Arbre des Altérations.csv"
@@ -35,17 +39,42 @@ t2_tri = t2.dropna(subset="Tier 2 Réactif 2")
 t3 = df.iloc[:, 8:12].dropna(subset="Tier 3 Produit")
 
 
+def assemble_dfs(liste: list) -> pd.DataFrame:
+    """
+    Return a dataframe with all alterations on a single set of columns.
+
+    Parameters
+    ----------
+    liste : list
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    dfs = []
+    for df in liste:
+        _ = df.copy()
+        _.columns = ["Alt 1", "Alt 2", "Alt 3", "Product"]
+        dfs.append(_)
+    return pd.concat(dfs, keys=["T1", "T2", "T3"])
+
+
+check = assemble_dfs([t1, t2, t3])
+
+
 def check_all_combinations() -> bool:
     """Return True if all combinations of reagents exist in the dataframe."""
     # call following function with appropriate parameters
-    return (check_2_reagents_combinations(t1_bi)
-            & check_2_reagents_combinations(t2_bi)
+    return (check_2_reagents_combinations(t1_bi, "T1")
+            & check_2_reagents_combinations(t2_bi, "T2")
             & check_3_reagents_combinations(t1_tri)
             & check_3_reagents_combinations(t2_tri)
             & check_3_reagents_combinations(t3))
 
 
-def check_2_reagents_combinations(df_: pd.DataFrame) -> bool:
+def check_2_reagents_combinations(df_: pd.DataFrame, tier: str) -> bool:
     """
     Return True if all combinations involving 2 alterations exist in the df.
 
@@ -55,20 +84,19 @@ def check_2_reagents_combinations(df_: pd.DataFrame) -> bool:
         True if all 2 alterations combinations are good, else False.
 
     """
-    cols = df_.columns.to_list()
-
     def to_apply(series):
-        reordered_series = pd.Series({cols[0]: series[cols[1]],
-                                      cols[1]: series[cols[0]],
-                                      cols[2]: series[cols[2]]})
-        return check_if_exists_in_df(reordered_series, df_)
-    exists = df_.apply(to_apply, axis=1)
+        reordered_series = pd.Series({"Alt 1": series["Alt 2"],
+                                      "Alt 2": series["Alt 1"],
+                                      "Alt 3": pd.NA,
+                                      "Product": series["Product"]})
+        return check_if_exists_in_df(reordered_series, check)
+    exists = check.apply(to_apply, axis=1)
     if exists.all():
         return True
     else:
         for idx, x in exists.items():
             if not x:
-                print(f"Series # {idx+2}: {list(df_.loc[idx, :].array)} "
+                print(f"Series # {idx[1]+2}: {list(check.loc[idx, :].array)} "
                       "has no equivalent.")
         return False
 
@@ -101,20 +129,21 @@ def check_3_reagents_combinations(df_: pd.DataFrame) -> bool:
 
     """
     missing_combinations = set()
+    df_.columns = ["Alt 1", "Alt 2", "Alt 3", "Product"]
 
     def to_apply(series):
         for reordered_series in get_tricombinations_series(series):
-            ret = check_if_exists_in_df(reordered_series, df_)
+            ret = check_if_exists_in_df(reordered_series, check)
             if not ret:
                 missing_combinations.add("Missing combination "
                                          f"{list(reordered_series.values)} "
                                          "corresponding to recipe "
-                                         f"line {df_[df_.eq(series).all(axis=1)].index[0]+2} "
+                                         f"line {check[check.eq(series).all(axis=1)].index[0][1]+2} "
                                          f"{list(series.values)}")
                 return False
             else:
                 return True
-    exists = df_.apply(to_apply, axis=1)
+    exists = df_.dropna(subset="Alt 3").apply(to_apply, axis=1)
     for _ in missing_combinations:
         print(_)
     if exists.all():
@@ -145,7 +174,8 @@ def get_autres(series: pd.Series, alteration_principale: str) -> tuple:
 
     """
     return tuple(sorted([_ for _ in list(series.values)
-                         if _ != alteration_principale]))
+                         if _ != alteration_principale],
+                        key=functools.cmp_to_key(locale.strcoll)))
 
 
 def get_tricombinations_series(s: pd.Series) -> list:
@@ -167,13 +197,11 @@ def get_tricombinations_series(s: pd.Series) -> list:
         List of reordered Series.
 
     """
-    # TODO: schéma : permutation r=1 + sort alphabetic des autres
-    cols = s.index.to_list()
     combinations = [_ + get_autres(s[:3], _[0])
                     for _ in itertools.permutations(list(s.values)[:3], r=1)
                     if _[0] != s[0]]
-    return [pd.Series({cols[0]: _[0], cols[1]: _[1], cols[2]: _[2],
-                       cols[3]: s[cols[3]]})
+    return [pd.Series({"Alt 1": _[0], "Alt 2": _[1], "Alt 3": _[2],
+                       "Product": s["Product"]})
             for _ in combinations]
 
 
@@ -182,7 +210,6 @@ def check_coherent_results(dataframe: pd.DataFrame) -> bool:
     # si les check d'avant passent, alors toutes recettes sont cohérentes
     # ssi il n'y a aucun doublons sur les recettes, càd aucun cas ou on a
     # 1 recette pour 2 produits
-    # TODO: check doublons pour 3 tiers
     dupes_index = dataframe.index[dataframe.duplicated(dataframe.columns[:-1],
                                                        keep=False)] + 2
     if not dupes_index.empty:
